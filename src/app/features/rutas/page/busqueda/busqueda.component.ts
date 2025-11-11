4f4eeb15aaed5ccbddf6ea425c211db0915bf94e
Moroff3 <Crostzard@outlook.com>
2025-11-11 12:54:34 -0500

    Archivos html, css y typescript para página de búsqueda de rutas

---
+++ src/app/features/rutas/page/busqueda/busqueda.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-busqueda-rutas',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './busqueda.component.html',
+  styleUrls: ['./busqueda.component.css']
+})
+export class BusquedaRutasComponent {}
