dc2091a5a22b7122e0ff552396c20ac1c6c47097
NicoleBances <nicolebances929@gmail.com>
2025-12-02 19:43:55 -0500

    Se corrigieron los nombres de los placeholders de todos los inputs de los formularios para crear cuentas

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 // Environment - Configuración para desarrollo
 export const environment = {
   production: false,
-  //apiUrl: 'http://localhost:8080/api/v1'
-  apiUrl: 'https://uniride-api-1-0.onrender.com/api/v1'
+  apiUrl: 'http://localhost:8080/api/v1'
+  //apiUrl: 'https://uniride-api-1-0.onrender.com/api/v1'
 };
